ba325a6eaa70e5a41a56e6bf7dc5d42263ae52ff
Mahdi Abdinejadi <gusabdinma@mltmac10.flov.gu.se>
2014-04-04 19:45:28 +0200

    Added author's names

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -1,1 +1,37 @@
-print "hello world"
+from __future__ import division
+'''
+Created on Apr 04, 2014
+
+Lab assignment 4 (VG1)
+
+@authors: Mahdi Abdinejadi, Mehdi Ghanimifard
+@version: 1.0
+'''
+
+
+'''
+import libraries
+'''
+
+'''
+import external modules
+'''
+
+
+'''
+Function definitions
+'''
+def main():
+	print "main Function"
+
+
+
+
+
+
+
+'''
+Global
+'''
+if __name__ == '__main__':
+	main()
